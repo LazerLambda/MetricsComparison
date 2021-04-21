d636ae18660bf9680898e992ff889cce44588334
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2021-04-21 22:45:38 +0200

    21.04.2021

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ pip install bert-score
 echo "Installing GRUEN"
 git clone https://github.com/WanzhengZhu/GRUEN
 cd GRUEN
-mkdir cola_model
 pip install -r requirements.txt
+python -m spacy download en_core_web_md
 cd ..
+echo "\n\nDownload CoLA classifier from this location: \033[94mhttps://drive.google.com/file/d/1Hw5na_Iy4-kGEoX60bD8vXYeJDQrzyj6/view\033[0m\n\t'-> \033[93mput the folder 'cola_model' in the GRUEN folder.\033[0m\n\n"
