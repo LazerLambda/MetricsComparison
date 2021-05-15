48837f1869e21b01be6ac067b9ae3c7722c299a5
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2021-05-15 15:41:34 +0200

    Debugged

--- DamageData.py
+++ DamageData.py
@@ -379,50 +379,49 @@ class DamageData:
 
     @staticmethod
     def drop_single_pos(sentence : str, doc : spacy.tokens.doc.Doc, pos : str) -> list:
-    """ Drop single word function
+        """ Drop single word function
 
-    Function to drop words based on their POS tag. 
+        Function to drop words based on their POS tag. 
 
-    Parameter
-    ---------
-    sentence : str
-        sentence to be deteriorated
-    doc : spacy.tokens.doc.Doc
-        spacy document, to extract the indices of the token from
-    pos : str
-        pos tag whose words will be dropped
+        Parameter
+        ---------
+        sentence : str
+            sentence to be deteriorated
+        doc : spacy.tokens.doc.Doc
+            spacy document, to extract the indices of the token from
+        pos : str
+            pos tag whose words will be dropped
 
-    Returns
-    -------
-    str
-        deteriorated sentence
-    """
+        Returns
+        -------
+        str
+            deteriorated sentence
+        """
 
-    candidates : list = []
+        candidates : list = []
 
-    for i in range(len(doc)):
+        for i in range(len(doc)):
 
-        if doc[i].pos_ == pos:
-            candidates.append(i)
-        else:
-            continue
-    
-    if len(candidates) == 0:
-        return sentence, False
-    
-    diff : int = 0
-    for i in candidates:
-        bounds = doc[i].idx - diff, doc[i].idx + len(doc[i].text) - diff
-        sentence = sentence[0:bounds[0]] + sentence[(bounds[1] + 1)::]
-        diff += len(doc[i].text) + 1
-    
+            if doc[i].pos_ == pos:
+                candidates.append(i)
+            else:
+                continue
+        
+        if len(candidates) == 0:
+            return sentence, False
+        
+        diff : int = 0
+        for i in candidates:
+            bounds = doc[i].idx - diff, doc[i].idx + len(doc[i].text) - diff
+            sentence = sentence[0:bounds[0]] + sentence[(bounds[1] + 1)::]
+            diff += len(doc[i].text) + 1
+        
 
-    return sentence, True
-    
+        return sentence, True
+        
 
 
-    @staticmethod
-    def pos_drop(sentences : list, pos : str):
+    def pos_drop(self, text : str,  probability : float, pos : str):
         """ POS drop function
 
         Function to drop random words with a specific POS-Tag from a sentence.
@@ -431,22 +430,39 @@ class DamageData:
         ---------
         sentences : list
             list of already tokenized sentence tokens
+        probability : float
+            float in [0,1]
+        pos : str
+            pos tag which occurences will be dropped
 
+        Raises
+        ------
+        Exception
+            if probability is not in [0,1]
+        
         Returns
         -------
         list
             list of sentences
         """
 
+        if probability < 0 or probability > 1:
+            raise Exception("Probability must be a number in [0,1].")
+
+        sentences = nltk.sent_tokenize(text)
+
         ret_list : list = []
         indices : list = []
 
+        # TODO pipe sentences
         for i, sentence in enumerate(sentences):
-                new_sentence = sentence
-                new_sentence, success = drop_single_pos(sentence=new_sentence, doc=nlp(new_sentence), pos=pos)
+                if bool(np.random.binomial(size=1, n=1, p= probability)):
+                    new_sentence, success = drop_single_pos(sentence=new_sentence, doc=self.nlp(new_sentence), pos=pos)
 
-                if success:
-                    indices.append(i)
-                ret_list.append(new_sentence)
+                    if success:
+                        indices.append(i)
+                    ret_list.append(new_sentence)
+                else:
+                    ret_list.append(sentence)
     
         return ret_list, indices
