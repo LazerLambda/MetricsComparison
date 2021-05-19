eb40a597a0c0b6e394c9d12024d81d6eb533b590
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2021-05-19 10:25:39 +0200

    Update Install script

--- install.sh
+++ install.sh
@@ -25,3 +25,9 @@ pip install -r requirements.txt
 python -m spacy download en_core_web_md
 cd ..
 echo "\n\nDownload CoLA classifier from this location: \033[94mhttps://drive.google.com/file/d/1Hw5na_Iy4-kGEoX60bD8vXYeJDQrzyj6/view\033[0m\n\t'-> \033[93mput the folder 'cola_model' in the GRUEN folder.\033[0m\n\n"
+
+echo "Downloading library specific data"
+python -m spacy download en_core_web_sm
+python -c "import nltk; nltk.download('punkt')"
+
+echo "DONE"
