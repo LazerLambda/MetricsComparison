616c92b0cc3c9df3a5ac7ef9c0fafd11f4c21390
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2021-06-18 15:49:39 +0200

    PlotByMetric.py documented and linted

--- src/PlotByMetric.py
+++ src/PlotByMetric.py
@@ -1,3 +1,5 @@
+"""This module plots different tasks by metric."""
+
 from .Plot import Plot
 
 import matplotlib.pyplot as plt
@@ -8,23 +10,28 @@ from functools import reduce
 
 
 class PlotByMetric(Plot):
+    """Class to organize plotting. Inherited from Plot class."""
 
-
-    def __init__(self, task_list : list, wd : str):
+    def __init__(self, task_list: list, wd: str):
+        """Do initialization of the class."""
         super(PlotByMetric, self).__init__(task_list=task_list, wd=wd)
 
-
-    def plot(self, metrics : list) -> None:
-
+    def plot(self, metrics: list) -> None:
+        """Plot data."""
         assert isinstance(metrics, list)
 
-        metric_list : list = reduce(lambda acc, metric: acc + list(zip(metric.submetrics, [metric for _ in metric.submetrics])), [metric for metric in metrics], [])
+        metric_list: list = reduce(
+            lambda acc, metric: acc +
+            list(zip(
+                metric.submetrics,
+                [metric for _ in metric.submetrics])),
+            [metric for metric in metrics], [])
 
         palette = sns.color_palette(None, len(self.task_list))
 
+        for _, (submetric, metric) in enumerate(metric_list):
 
-        for i, (submetric, metric) in enumerate(metric_list):
-
+            # TODO annotate
             x, y = Plot.find_square(len(self.task_list))
             _, axes = plt.subplots(nrows=y, ncols=x, figsize=(20, 10))
 
@@ -33,21 +40,21 @@ class PlotByMetric(Plot):
                     axes = [[axes]]
                 else:
                     axes = [axes]
-                
 
-            for j, (task, df, name, descr) in enumerate(self.task_list):
+            for j, (task, _, name, descr) in enumerate(self.task_list):
 
-                x_i = j % x
-                y_i = j // x 
+                x_i: int = j % x
+                y_i: int = j // x
 
+                # TODO annotate
                 ax = axes[y_i][x_i]
 
                 task.plot(ax, metric, submetric, color=palette[j])
-                ax.legend(bbox_to_anchor=(1,0), loc="lower left")
-                ax.set_title(descr,fontweight="bold", size=10)
+                ax.legend(bbox_to_anchor=(1, 0), loc="lower left")
+                ax.set_title(descr, fontweight="bold", size=10)
 
             plt.suptitle(metric.name)
             plt.tight_layout()
-            path : str = os.path.join(self.wd, name + "_plot_by_metric.png")
+            path: str = os.path.join(self.wd, name + "_plot_by_metric.png")
             plt.savefig(path)
             plt.show()
