e055fb60d5ce982eb0e5e49bb03e5cff5ebc474f
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2021-06-26 18:57:46 +0200

    Update experiment parameters

--- BERTScore_exp_screen_script.sh
+++ BERTScore_exp_screen_script.sh
@@ -1,6 +1,6 @@
 
 screen -dmS BERTScore
-screen -S BERTScore -p 0 -X stuff 'python3.8 BERTScore_entry.py -n 500 -sd 42 -s 4 -t BERTScore500\n'
+screen -S BERTScore -p 0 -X stuff 'python3.8 BERTScore_entry.py -n 2000 -sd 42 -s 4 -t BERTScore2000\n'
 
 screen -dmS BERTScoreIDF
-screen -S BERTScoreIDF -p 0 -X stuff 'python3.8 BERTScore_idf_entry.py -n 500 -sd 42 -s 4 -t BERTScoreIDF500\n'
+screen -S BERTScoreIDF -p 0 -X stuff 'python3.8 BERTScore_idf_entry.py -n 2000 -sd 42 -s 4 -t BERTScoreIDF2000\n'
