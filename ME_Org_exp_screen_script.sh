5e2fad2cd6f250dad848e989d777721922ed1c61
Philipp Koch <PhillKoch@protonmail.com>
2021-07-02 08:20:57 +0000

    Commit before pull

--- ME_Org_exp_screen_script.sh
+++ ME_Org_exp_screen_script.sh
@@ -1,6 +1,6 @@
 
 screen -dmS MEOrgBERT
-screen -S MEOrgBERT -p 0 -X stuff 'python3.8 MEOrg_BERT_entry.py -n 100 -sd 42 -s 4 -t MEBERT100\n'
+screen -S MEOrgBERT -p 0 -X stuff 'python3.8 MEOrg_BERT_entry.py -n 500 -sd 42 -s 4 -t MEBERT100\n'
 
 screen -dmS MEOrgSBERT
-screen -S MEOrgSBERT -p 0 -X stuff 'python3.8 MEOrg_SBERT_entry.py -n 2000 -sd 42 -s 4 -t MESBERT2000\n'
+screen -S MEOrgSBERT -p 0 -X stuff 'python3.8 MEOrg_SBERT_entry.py -n 100 -sd 42 -s 4 -t MESBERT2000\n'
