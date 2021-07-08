051fbb1d341f6f5227d8d80f60d1f2a51938a547
LazerLambda <49939753+LazerLambda@users.noreply.github.com>
2021-07-08 12:05:33 +0200

    Commit before pull

--- run_all_screens.sh
+++ run_all_screens.sh
@@ -1,15 +1,15 @@
 
 screen -dmS METhBERT_R
-screen -S METhBERT_R -p 0 -X stuff 'python3.8 METhBERT_entry.py -n 100 -sd 42 -s 4 -t MEBERT100\n'
+screen -S METhBERT_R -p 0 -X stuff 'python3.8 METh_BERT_entry.py -n 100 -sd 42 -s 4 -t MEBERT100\n'
 
 screen -dmS METhSBERT_R
-screen -S METhSBERT_R -p 0 -X stuff 'python3.8 METhSBERT_entry.py -n 2000 -sd 42 -s 4 -t MESBERT2000\n'
+screen -S METhSBERT_R -p 0 -X stuff 'python3.8 METh_SBERT_entry.py -n 2000 -sd 42 -s 4 -t MESBERT2000\n'
 
 screen -dmS MEOrgBERT_R
-screen -S MEOrgBERT_R -p 0 -X stuff 'python3.8 MEOrg_BERT_entry.py -n 500 -sd 42 -s 4 -t MEBERT500\n'
+screen -S MEOrgBERT_R -p 0 -X stuff 'python3.8 MEOrg_BERT_entry.py -n 500 -sd 42 -s 4 -t MEOrgBERT500\n'
 
 screen -dmS MEOrgSBERT_R
-screen -S MEOrgSBERT_R -p 0 -X stuff 'python3.8 MEOrg_SBERT_entry.py -n 2000 -sd 42 -s 4 -t MESBERT2000\n'
+screen -S MEOrgSBERT_R -p 0 -X stuff 'python3.8 MEOrg_SBERT_entry.py -n 2000 -sd 42 -s 4 -t MEOrgSBERT2000\n'
 
 screen -dmS BLEURT_R
 screen -S BLEURT_R -p 0 -X stuff 'python3.8 BLEURT_entry.py -n 2000 -sd 42 -s 4 -t BLEURT2000\n'
